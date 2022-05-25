9dabedeef053fde5b4a4c34544eddf0bedd16456
Alexandre Lira <alexandrelira112@gmail.com>
2022-05-25 19:35:50 -0300

    input of modal, close butto and submit button

--- src/components/NewTransactionModal/NewTransactionModal.tsx
+++ src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -1,19 +1,39 @@
 import Modal from 'react-modal';
+import { close } from '../../@share/images/shareImages';
+import { Container } from './NewTransactionModalStyles';
 
 interface NewTransactionModalProps {
-    isOpen: boolean;
-    onRequestClose: () => void;
+  isOpen: boolean;
+  onRequestClose: () => void;
 }
 
 export function NewTransactionModal(props: NewTransactionModalProps) {
-    const {isOpen, onRequestClose} = props
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
+  const { isOpen, onRequestClose } = props
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
 
-        >
-            <h1>cadastrar transação</h1>
-        </Modal>
-    );
+      <button 
+        type="button" 
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={close} alt="Fechar modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder="Título"/>
+        <input type="number" placeholder="Valor"/>
+        <input type="text" placeholder="Categoria"/>
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
+    </Modal>
+  );
 }
